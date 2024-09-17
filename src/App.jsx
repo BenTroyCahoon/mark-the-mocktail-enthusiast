d174dfa4637f14c407d88f927cdbf2fb2a53d0c1
BenTroyCahoon <benjamin.cahoon@hotmail.com>
2024-09-17 14:13:30 +0200

    NEW NAME
    secret

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,10 @@ function App() {
     initialDrinks
   );
   const [activeFilter, setActiveFilter] = useState(() => (drink) => drink); //returns a 1 to 1 function
-
+secret key = 123,
+  console.log(" hejsan hoppsan")
+  
+  console.log()
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(
